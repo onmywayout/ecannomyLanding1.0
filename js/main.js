b8cf3f479749ddfc31b37678a9d1015bfeb50425
Nicolas Zapata <nicolaszapata@gmail.com>
2019-05-07 10:46:10 -0500

    Version 1.01

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/**
+		 * Enviar formulario de contacto y mostrar respuesta
+		 */
+		function enviarForm()
+		{
+
+		   
+		        var telefono = document.getElementById("telefono").value;
+                     nombre = document.getElementById("nombre").value,
+		            email = document.getElementById("email").value,
+		            empresa = document.getElementById("empresa").value;
+		            
+
+		            if (!email) //la función se llama sola al cargar la página con campos vacios
+		                return;
+
+		        $.ajax({
+		            type: "POST",
+		            url: 'https://o6ur6ye60l.execute-api.us-east-1.amazonaws.com/prod/processContactFormRequest',
+		            contentType: 'application/json',
+		            data: JSON.stringify({
+		                'nombre': nombre,
+		                'email': email,
+		                'telefono': telefono,
+		                'empresa': empresa
+		            }),
+		            success: function(res)
+		            {
+		                alert("Sus datos han sido enviados correctamente");
+		            },
+		            error: function(res)
+		            {
+		                alert("Lo lamentamos mucho. Ha ocurrido un error");
+		                console.log(res);
+		            }
+		        });
+		        return;
+
+		}
+
+		/**
+		 * Vuelve a habilitar el form para que se pueda enviar otro mensaje despues del primero
+		 */
+		function mostrarForm()
+		{
+		    $("#formulario").removeClass("ocultar");
+		    $("#respuesta").addClass("ocultar");
+		    return;
+		}
